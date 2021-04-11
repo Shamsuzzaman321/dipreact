ba00ca63eace575333bca7c097a7c3d8d600e82f
Md.Shamsuzzaman <dip.cse.nsu@gmail.com>
2021-04-11 23:31:37 +0600

    updated

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h1>welcome to the react js</h1>
+        <h2>এইখান থেকে আমরা reactjs এর project এর কাজ শুরু করবো </h2>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React From the very scratch and we will see in details how these works
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
