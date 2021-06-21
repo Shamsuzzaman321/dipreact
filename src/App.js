7c81e6c5abb7be6cf4b544517b5f219e3ce70f52
Md.Shamsuzzaman <dip.cse.nsu@gmail.com>
2021-06-21 23:57:09 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <h1>welcome to the react js</h1>
         <h2>এইখান থেকে আমরা reactjs এর project এর কাজ শুরু করবো </h2>
         <h3>React ব্যবহার করার জন্য জাভাস্ক্রিপ্ট জানা টা বেশ জরুরি বিশেষ করে মডার্ন জাভাস্ক্রিপ্ট সিন্টেক্স </h3>
-        <h3>this project has been deployed at netlify</h3>
+        <h3>this project has been deployed at netlify and netlify is very fast </h3>
 
 
         <a
