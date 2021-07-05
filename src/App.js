85ff1fba68d63d2af8e990a25f2565a8a19f150c
Md.Shamsuzzaman <dip.cse.nsu@gmail.com>
2021-07-06 01:22:25 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -6,13 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <h4>welcome to the react js this is my react tutorial by bulbulahmed</h4> 
         <h2>এইখান থেকে আমরা reactjs এর project এর কাজ শুরু করবো </h2>
         <h3>React ব্যবহার করার জন্য জাভাস্ক্রিপ্ট জানা টা বেশ জরুরি বিশেষ করে মডার্ন জাভাস্ক্রিপ্ট সিন্টেক্স </h3>
         <h3>this project has been deployed at netlify and netlify is very fast </h3>
+        
         </header>
     </div>
   );
