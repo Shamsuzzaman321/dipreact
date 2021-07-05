708798eb42ba4a8f96ee8049d337831ce41ff1a5
Md.Shamsuzzaman <dip.cse.nsu@gmail.com>
2021-07-06 00:08:58 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <h1>welcome to the react js</h1>
+        <h1>welcome to the react js this is my react tutorial by bulbulahmed</h1> 
         <h2>এইখান থেকে আমরা reactjs এর project এর কাজ শুরু করবো </h2>
         <h3>React ব্যবহার করার জন্য জাভাস্ক্রিপ্ট জানা টা বেশ জরুরি বিশেষ করে মডার্ন জাভাস্ক্রিপ্ট সিন্টেক্স </h3>
         <h3>this project has been deployed at netlify and netlify is very fast </h3>
@@ -29,3 +29,4 @@ function App() {
 }
 
 export default App;
+
