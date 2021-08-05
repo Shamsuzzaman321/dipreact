4a6d24ed6d76407cfd92d5d8322e1ff6d0b7fd43
Shamsuzzaman321 <dip.cse.nsu@gmail.com>
2021-08-05 15:33:36 +0600

    function components and class components added successfully

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Functionalcomp from './components/Functionalcomp';
+import Classcomp from './components/Classcomp';
 function App() {
   return (
     <div className="App">
 
     <Functionalcomp />  
+    <Classcomp />
 
-    
+
     </div>
   );
 }
---
+++ src/components/Classcomp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class Classcomp extends React.Component {
+	render() {
+		return (
+			<div>
+			<h1>Hello world this is my Class components</h1>	
+			</div>
+		)
+	}
+}
+
