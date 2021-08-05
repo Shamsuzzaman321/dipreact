a9f07814e7fa211f7212bde2a52a88183c475021
Shamsuzzaman321 <dip.cse.nsu@gmail.com>
2021-08-05 15:37:15 +0600

    function components and class components declaration updated successfully

--- src/components/Classcomp.js
+++ src/components/Classcomp.js
@@ -4,7 +4,9 @@ export default class Classcomp extends React.Component {
 	render() {
 		return (
 			<div>
-			<h1>Hello world this is my Class components</h1>	
+			<h1>Hello world this is my Class components</h1>
+			<h2>This a child component which is put into mother component named App.js</h2>
+	
 			</div>
 		)
 	}
--- src/components/Functionalcomp.js
+++ src/components/Functionalcomp.js
@@ -5,7 +5,8 @@ import React from 'react'
 const Functionalcomp = () => {
 	return (
 		<div>
-			<h1>Hello world this is my arrowfunction components</h1>
+			<h1>Hello world this is my function components</h1>
+			<h2>This a child component which is put into mother component named App.js</h2>
 		</div>
 	)
 }
