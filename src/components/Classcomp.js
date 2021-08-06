4045116ccd1cf723e5463ddd9ba1c40404b50719
Shamsuzzaman321 <dip.cse.nsu@gmail.com>
2021-08-07 03:22:02 +0600

    State components has been added

--- src/components/Classcomp.js
+++ src/components/Classcomp.js
@@ -6,7 +6,7 @@ export default class Classcomp extends React.Component {
 			<div>
 			<h1>Hello world this is my Class components</h1>
 			<h2>This a child component which is put into mother component named App.js</h2>
-	      	<h2>নাম={this.props.name} বয়স={this.props.age} জন্মস্থান={this.props.birthplace}</h2>
+	      	<h2>by using props(classcomponent)===>নাম={this.props.name} বয়স={this.props.age} জন্মস্থান={this.props.birthplace}</h2>
 
 			</div>
 		)
---
+++ src/components/Statecomp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Statecomp = () => {
+	return (
+		<div>
+		<h1>======================State========================</h1>
+		<br />	
+		</div>
+	)
+}
+
+export default Statecomp
