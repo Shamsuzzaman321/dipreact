2e88d5bd9dd83f7a5082349af8870643d3ce4519
Shamsuzzaman321 <dip.cse.nsu@gmail.com>
2021-08-08 01:48:05 +0600

    Reactjs State added successfully

--- src/components/Statecomp.js
+++ src/components/Statecomp.js
@@ -21,6 +21,7 @@ class Statecomp extends React.Component {
     <div>
       <h1>========================State======================</h1>
       <h2>In Here we will learn details about State</h2>
+      <h2>In Here we will learn details about setState</h2>
       <h1>{this.state.name}</h1>
       <h2>In Here we will see the {this.state.name}</h2>
       <h2>In Here we will see the {this.state.name}</h2>
