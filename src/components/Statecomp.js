db8f76ece7aa943023a635b224933e5fe44f0b6b
Shamsuzzaman321 <dip.cse.nsu@gmail.com>
2021-08-09 00:24:47 +0600

    state features has been updated successfully

--- src/components/Statecomp.js
+++ src/components/Statecomp.js
@@ -6,25 +6,37 @@ class Statecomp extends React.Component {
   
   	this.state = {
   		 name: "Bangladesh",
+       age: 100
   	}
   }
 
-  function changestatrevariables () {
-	this.setState({
-  		 name: "updated Statevariable"
+  setStatefunction =()=> {
 
-	})
-  }
+    this.setState({
+      name: "America",
+       age: 70
+    })
+
+  // this.setState={
+  //      name: "America",
+  //      age: 70
+  //  // body... 
+  // }
+ }
+
+  
 
   render() {
     return (
     <div>
       <h1>========================State======================</h1>
-      <h2>In Here we will learn details about State</h2>
-      <h2>In Here we will learn details about setState</h2>
-      <h1>{this.state.name}</h1>
-      <h2>In Here we will see the {this.state.name}</h2>
-      <h2>In Here we will see the {this.state.name}</h2>
+      <h2>In Here we will learn details about State(general variable declarations with value)</h2>
+      <h2>In Here we will learn details about setState(update variable's value)</h2>
+      <h1>in here we are showing the output of the declared variables={this.state.name}</h1>
+      <h1>in here we are showing the output of the declared variables={this.state.age}</h1>
+      <h1>updated output of the declared variables={this.setState.name}</h1>
+      <button onClick>click me</button>
+      
     </div>
     );
   }
