43c9a4f2103c62b5a3917f95bee5b9132c4b8e29
Md.Shamsuzzaman <dip.cse.nsu@gmail.com>
2021-08-14 23:54:48 +0600

    updated

--- src/components/Prntmthdasprops.js
+++ src/components/Prntmthdasprops.js
@@ -19,7 +19,12 @@ class Prntmthdasprops extends React.Component {
       <h1>==Method as props,send data(child=mother) as argument value passing==</h1>
       <h2>======We will pass data via method/function(declare function) ======</h2>
       <Chldmthdasprops childvarname={this.state.varname}/>
-      <Chldmthdasprops childfuncname={ this.prntfunctioname}/>
+      <Chldmthdasprops childfuncname={this.prntfunctioname}/> 
+      {/* components(childcomponent) ke jokhn amra html ending tag akare prokash */}
+      {/*  korbo  mother root components e tokhn this {this.varname/functionname} tai simple way te likhbo */}
+      {/*   */}
+      {/*  bind event handler arrorfunction shudhumatro event handler ei use hoye  */}
+      {/* thake syntex onClick={()=>this.func/var/class object/method name()} */}
 
       </div>
     );
