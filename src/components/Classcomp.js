86294ecc7781cc1939234bf347c3388882d76fbe
Md.Shamsuzzaman <dip.cse.nsu@gmail.com>
2021-08-15 23:24:12 +0600

    Conditional rendering features added

--- src/components/Classcomp.js
+++ src/components/Classcomp.js
@@ -4,7 +4,7 @@ export default class Classcomp extends React.Component {
 	render() {
 		return (
 			<div>
-			<h1>Hello world this is my Class components</h1>
+			<h1>Hello world this is my Class components which is a stateful components that contains life cycle method</h1>
 			<h2>This a child component which is put into mother component named App.js</h2>
 	      	<h2>by using props(classcomponent)===>নাম={this.props.name} বয়স={this.props.age} জন্মস্থান={this.props.birthplace}</h2>
 
