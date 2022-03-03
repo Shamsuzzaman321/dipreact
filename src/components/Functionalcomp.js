e456460cbf62e3fa5a7009f499cb69b367aef44c
Md.Shamsuzzaman <dip.cse.nsu@gmail.com>
2022-03-03 15:13:25 +0600

    updated

--- src/components/Functionalcomp.js
+++ src/components/Functionalcomp.js
@@ -6,6 +6,7 @@ const Functionalcomp = () => {
 	return (
 		<div>
 			<h1>Hello world this is my function components</h1>
+			<h1>In the Future We not gonna use any class component</h1>
 			<h2>This a child component which is put into mother component named App.js</h2>
 		</div>
 	)
