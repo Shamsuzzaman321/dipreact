4aeb142bb14689bccd4f3f4d4108212df767a210
Shamsuzzaman321 <dip.cse.nsu@gmail.com>
2022-04-03 02:04:18 +0600

    updated

--- src/components/Classcomp.js
+++ src/components/Classcomp.js
@@ -4,6 +4,7 @@ export default class Classcomp extends React.Component {
 	render() {
 		return (
 			<div>
+				<h1>ব্লকচেইন এ মাস্টার হতে গেলে আমাদের অবশ্যই MERN জাভাস্ক্রিপ্ট টেকনোলজি সম্পর্কে details and depth knowledge থাকতে হবে </h1>
 			<h1>Hello world this is my Class components which is a stateful components that contains life cycle method</h1>
 			<h2>This a child component which is put into mother component named App.js</h2>
 	      	<h2>by using props(classcomponent)===>নাম={this.props.name} বয়স={this.props.age} জন্মস্থান={this.props.birthplace}</h2>
